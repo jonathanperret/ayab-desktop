0909a8c3b5de967ce8514eaad2adab68c1cf69ed
jonathanperret <j-github@jonathanperret.net>
2024-07-24 10:09:27 +0000

    Deploying to gh-pages from @ jonathanperret/ayab-desktop@a2f14e74ce6d9000edd0fcc736837d26caf1c1a8 🚀

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['changelog_2',['Changelog',['../md_CHANGELOG.html',1,'']]],
-  ['contributing_20to_20ayab_3',['Contributing to AYAB',['../md_CONTRIBUTING.html',1,'']]]
+  ['changelog_2',['Changelog',['../md_CHANGELOG.html',1,'']]]
 ];
